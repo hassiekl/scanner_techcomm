ebb5c89efbc13d56c3710479da8a57a7262ba693
Jonathan M. Torres <mydiaxd088@gmail.com>
2017-10-14 19:46:21 -0700

    Update DIRECTAXIS.cpp

--- DIRECTAXIS.cpp
+++ DIRECTAXIS.cpp
@@ -6,32 +6,7 @@ THE COMPUTER CANNOT DECRYPT IT ITS A SPECIAL KEY AND THE LETTERS BEING OUTPUTTED
 TO MAKE A NEST OF ALL THE PROGRAMS THAT COULD BE CREATED USING LETTERS AND SYMBOLS AS EVENTS AND TRIGGERS FOR BUILDING A PROGRAM
 FROM NOTHING AS IMPORTING A KEY INSTEAD OF A TORRENT, THEN BUILDING IT AS A KEY. !!
 
-    Testing "hello world", and "world"
-    provides two different outputs
-    for the ending string memory size.
-    These memory sizes are apart of the string
-    they allocate memory and are used for mathematical purposes
-    these are random and they hold information.
-    I completed apples to oranges in float and integer.
-    Back to memory. These are initiated arrays built from
-    float data type to a cycle and back to a string to see
-    the machine language in ASCII. The integer values are
-    encrypted and need to be decrypted, but they are just floating
-    memory space and is empty. They do not use anything but a validated
-    memory. Notice the initialized Alpha = 0, creates this
-    and without it, it is nothing. Therefore it is a stop code.
-    dir.Axis = ""; means it is reset, and if you put it back in
-    the memory is still random. The if statement in main is the trigger
-    and the event for the floating types to integer and string. They are compared
-    and are seen as apples to apples, instead of apples to oranges.
-    Testing single and double digits, is not correct in mathematical sequences.
-    They are just a way of encrypting and have no real sequence.
-    Meaning this could be the SHA we are looking for.
-    Notice that it builds the ASCII within itself, therefore
-    the computer language builds itself. Also notice when converting back to int,
-    the computer doesn't recognize the ASCII value anymore, therefore it
-    is pure memory and memory is now a string value that changes. Or holds a letter.
-    As seen in ajaxtyping.cpp..
+ TESTING HELLO WORLD, PLEASE INPUT A NUMBER BEFOREHAND AND THEN RETYPE THE INPUT SUCH AS "HELLO WORLD", "HELLO, WORLD".
 */
 
 
